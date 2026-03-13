877cc704892a0db4ade90c16e2dad1892b12ca69
rafiammar014 <rafiammar014@gmail.com>
2026-03-13 15:17:12 +0800

    add input procedure to prompt user for array elements

--- praktikum4algo.cpp
+++ praktikum4algo.cpp
@@ -16,3 +16,14 @@ void input (){ //procedure input
             cout << "\nArray Tidak Boleh Lebih dari 20\n";
         }
     }
+
+    cout << endl;
+    cout << "==========================================" << endl;
+    cout << "=========Masukan Element Array============" << endl;
+    cout << "==========================================" << endl;
+
+    for (int i = 0; i < n; i++){
+        cout << "Data ke-" << (i+1) << ": ";
+        cin >> arr[i];
+    }
+}
